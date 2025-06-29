af9cd5acbbb93ff5d2fd937392184e47f2a55516
Prince Bhanderi <bhanderiprince03@gmail.com>
2025-06-29 21:18:51 +0530

    Update CORS setup for Render deployment

--- server/index.js
+++ server/index.js
@@ -23,7 +23,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin: [
+      process.env.FRONTEND_URL || "http://localhost:3000",
+      "http://localhost:3000"
+    ],
 		credentials:true,
 	})
 )
